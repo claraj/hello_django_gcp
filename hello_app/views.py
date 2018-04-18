496e3183b8c18daf4204c8d0c5ac370d5a89d188
Clara <minneapolis-edu@users.noreply.github.com>
2018-04-18 08:36:32 -0500

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse('This app is deployed on App Engine!!')
+    return HttpResponse('Hello GCP!')
