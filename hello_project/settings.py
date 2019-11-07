16c73ee4ec0ac3f048754ec58b62c33a801bb0a3
clara <clara@minneapolis.edu>
2019-11-07 09:16:37 -0600

    bump django version

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -71,15 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hello_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
